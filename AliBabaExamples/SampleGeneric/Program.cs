8aba899ab982ff6b46729619423a6d7d8cd52b18
MostafaAbbasi <40124283+mostafaabbasi@users.noreply.github.com>
2022-05-22 23:03:34 +0430

    Add files via upload
    FirstCommit

---
+++ AliBabaExamples/SampleGeneric/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace SampleGeneric
+{
+    public interface IGenericArray
+    {
+        string Title { get; set; }
+    }
+    public class Person : IGenericArray
+    {
+        public string Title { get; set; }
+    }
+
+    public class MyGenericArray<T> where T : IGenericArray, new()
+    {
+
+        private T[] array;
+
+        public MyGenericArray(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getItem(int index)
+        {
+            return array[index];
+        }
+        public void setItem(int index, string value)
+        {
+            T a = new T();
+            a.Title = value;
+            array[index] = a;
+        }
+        public string GetName(int index)
+        {
+            var x = array[index];
+            return x.Title;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyGenericArray<Person> intArray = new MyGenericArray<Person>(5);
+
+
+
+            for (int c = 0; c < 5; c++)
+            {
+                intArray.setItem(c, string.Format("Ali {0}", c));
+            }
+
+
+            for (int c = 0; c < 5; c++)
+            {
+                Console.Write(intArray.GetName(c) + " ");
+            }
+
+            //Console.WriteLine();
+
+
+            //MyGenericArray<char> charArray = new MyGenericArray<char>(5);
+
+
+            //for (int c = 0; c < 5; c++)
+            //{
+            //    charArray.setItem(c, (char)(c + 97));
+            //}
+
+
+            //for (int c = 0; c < 5; c++)
+            //{
+            //    Console.Write(charArray.getItem(c) + " ");
+            //}
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AliBabaExamples/SampleOperator/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace SampleOperator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var book = new Book()
+            {
+                Name = "Mostafa",
+                Author = "Cloner",
+                CreateDate = DateTime.Now
+            };
+
+            //NoteBook noteBook = new NoteBook();
+
+            //noteBook = book;
+
+            var noteBook = (NoteBook)book;
+
+            Console.WriteLine("Name : {0} , Author : {1}", noteBook.Name, noteBook.Author);
+
+        }
+
+    }
+}
+
+public class Book
+{
+    public string Name { get; set; }
+
+    public string Author { get; set; }
+
+    public DateTime CreateDate { get; set; }
+}
+
+public class NoteBook
+{
+    public string Name { get; set; }
+
+    public string Author { get; set; }
+
+    public static explicit operator NoteBook(Book book)
+    {
+        return new NoteBook()
+        {
+            Author = book.Author,
+            Name = book.Name
+        };
+    }
+
+    //public static implicit operator NoteBook(Book book)
+    //{
+    //    return new NoteBook()
+    //    {
+    //        Author = book.Author,
+    //        Name = book.Name
+    //    };
+    //}
+}
